05d02b8851da27bde6eb220d47ff77886178d53e
Admin <mostafajebriel@gmail.com>
2022-11-14 20:21:42 +0200

    q10

---
+++ lib/home.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
+
+
+class Home extends StatefulWidget {
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+ bool isb=true;
+ var primaryColor = Color(0xFFE0E0E0);
+
+  @override
+  Widget build(BuildContext context) {
+    Offset o=isb?Offset(10,10):Offset(28,28);
+    double b=isb?5.0:30.0;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("All Widget 2"),
+        centerTitle: true,
+        backgroundColor: Colors.greenAccent,
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("All Widget 2",style: TextStyle(fontWeight: FontWeight.bold,
+              fontSize: 30),),
+              SizedBox(height: 20,),
+              GestureDetector(
+                onTap: ()=>setState(()=>isb=!isb),
+                child: AnimatedContainer(
+                  duration: Duration(milliseconds: 100),
+                  child: SizedBox(height: 100,width: 100,),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                          offset: -o,
+                          color: Colors.white,
+                          blurRadius: b,
+                        inset: isb,
+                      ),
+                      BoxShadow(
+                        offset: o,
+                        color: Color(0xffa7a9af),
+                        blurRadius: b,
+                        inset: isb,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+            width: 150,
+            height: 150,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: primaryColor,
+              boxShadow: const [
+                BoxShadow(
+                  offset: Offset(-20, -20),
+                  blurRadius: 60,
+                  color: Colors.white,
+                  inset: true,
+                ),
+                BoxShadow(
+                  offset: Offset(20, 20),
+                  blurRadius: 60,
+                  color: Color(0xFFBEBEBE),
+                  inset: true,
+                ),
+              ],
+            ),
+          ),
+              SizedBox(height: 20,),
+
+              SizedBox(height: 20,),
+              SizedBox(height: 20,),
+              SizedBox(height: 20,),
+              SizedBox(height: 20,),
+              SizedBox(height: 20,),
+              SizedBox(height: 20,),
+              SizedBox(height: 20,),
+              SizedBox(height: 20,),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
